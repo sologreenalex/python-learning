8459003f7ed393a7e422f28dd0b9e9a4811cf641
sologreenalex <aymanalex00@gmail.com>
2026-09-20 00:24:24 +0300

    Add variables and data types lesson

---
+++ variables-data-types/lesson_01.py
@@ -0,0 +1,10 @@
+#تمارين علي المتغيرات  و دمج النصوص 
+
+name="aymen"
+age=49
+city="Alexandria"
+weight=110
+is_job=True
+future_age=5
+
+print(f"My Name is : {name} , I am {age} years old , I live in {city} , my weight is {weight}, Yes I have Job {is_job}, After 5 years my age is {age+future_age}")
